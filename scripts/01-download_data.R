7220c2e167bbc7407f62efb3fb6ff3e25b2e4d7f
David James <123336283+davidjamesdimalanta@users.noreply.github.com>
2024-04-24 06:27:22 -0400

    deleted OSSLT dataset

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
-# Purpose: Downloads and saves OSSLT scores from Data Ontario and PISA scores from the Learning Tower package
+# Purpose: Downloads and saves PISA scores from the Learning Tower package
 # Author: David James Dimalanta
-# Date: 27 March 2024
+# Date: 24 April 2024
 # Contact: david.dimalanta@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: none
@@ -10,25 +10,10 @@
 ### Workspace Setup###
 library(learningtower)
 
-#### Download PISA data ####
+#### Download PISA data from Learning Tower ####
 raw_data <- load_student("all")
 
 data(countrycode)
 
-# download OSSLT 2017-2018 data through URL
-"https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/3d8e7824-d033-4ca5-a5de-aa0dbec99002/download/sif_data_table_2017_2018_en.xlsx"
-
-# download OSSLT 2018-2019 data through URL
-"https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/bbbac037-b1c9-4b32-8f67-4fed1dfcac06/download/sif_data_table_2018_2019_en.xlsx"
-
-# download OSSLT 2019-2020 data through URL
-"https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/602a5186-67f5-4faf-94f3-7c61ffc4719a/download/sif_data_table_2019_2020_en.xlsx"
-
-# download OSSLT 2020-2021 data through URL
-"https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/600ab128-f50f-43a4-b4d4-97c39bb270b6/download/sif_data_table_2020_2021_en.xlsx"
-
-# download OSSLT 2021-2023 data through URL
-"https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/e0e90bd5-d662-401a-a6d2-60d69ac89d14/download/new_sif_data_table_2021_22prelim_en_march2024.xlsx"
-
-# download Distribution of total and current expenditure by educational institutions, from public and private sources, by level of education - Dataset frmo Government of Canada
-"https://www150.statcan.gc.ca/n1/tbl/csv/37100212-eng.zip"
+# download federal education spending from The World Bank by URL
+"https://api.worldbank.org/v2/en/indicator/SE.XPD.TOTL.GB.ZS?downloadformat=csv"
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Clean OSSLT data from Data Ontario, Education Policy Spending data from the Government of Canada, and PISA scores data from the OECD
+# Purpose: Clean Education Policy Spending data from the World Bank Data, and PISA scores data from the OECD
 # Author: David James Dimalanta
 # Date: 27 March 2024
 # Contact: david.dimalanta@mail.utoronto.ca
@@ -10,23 +10,23 @@
 library(readxl)
 library(tidyverse)
 library(janitor)
-library(arrow)
 library(learningtower)
+library(knitr)
+library(arrow)
 #### Clean data ####
 
 # read in data
-raw2017 <- read_xlsx("./data/raw_data/2017_2018.xlsx")
-raw2018 <- read_xlsx("./data/raw_data/2018_2019.xlsx")
-raw2019 <- read_xlsx("./data/raw_data/2019_2020.xlsx")
-raw2020 <- read_xlsx("./data/raw_data/2020_2021.xlsx")
-raw2021 <- read_xlsx("./data/raw_data/2021_2023.xlsx")
 raw_gov <- read_csv("./data/raw_data/37100212.csv")
 raw_data <- load_student("all")
+raw_spending <- read_csv("./data/raw_data/wbd.csv")
 
-### clean and filter PISA data
+# clean and filter PISA data
 pisa_data <- raw_data |>
   select(year, country, math, read, science) |>
-  filter(country == "FIN" | country == "HKG" | country == "CAN") |>
+  filter(country == "FIN" |
+           country == "HKG" |
+           country == "CAN" |
+           country == "CHL") |>
   na.omit()
 
 fin_data <- pisa_data |>
@@ -37,6 +37,9 @@ can_data <- pisa_data |>
 
 hkg_data <- pisa_data |>
   filter(country == "HKG")
+
+chl_data <- pisa_data |>
+  filter(country == "CHL")
 
 fin_read <- fin_data |>
   group_by(year) |>
@@ -55,6 +58,14 @@ can_read <- can_data |>
   )
 
 hkg_read <- hkg_data |>
+  group_by(year) |>
+  summarise(
+    lowest = min(read, na.rm = TRUE),
+    highest = max(read, na.rm = TRUE),
+    average = mean(read, na.rm = TRUE)
+  )
+
+chl_read <- chl_data |>
   group_by(year) |>
   summarise(
     lowest = min(read, na.rm = TRUE),
@@ -86,6 +97,14 @@ hkg_math <- hkg_data |>
     average = mean(math, na.rm = TRUE)
   )
 
+chl_math <- chl_data |>
+  group_by(year) |>
+  summarise(
+    lowest = min(math, na.rm = TRUE),
+    highest = max(math, na.rm = TRUE),
+    average = mean(math, na.rm = TRUE)
+  )
+
 fin_science <- fin_data |>
   group_by(year) |>
   summarise(
@@ -110,458 +129,60 @@ hkg_science <- hkg_data |>
     average = mean(science, na.rm = TRUE)
   )
 
+chl_science <- chl_data |>
+  group_by(year) |>
+  summarise(
+    lowest = min(science, na.rm = TRUE),
+    highest = max(science, na.rm = TRUE),
+    average = mean(science, na.rm = TRUE)
+  )
 
-#
-# make OSSLT 2017 data readable
-clean2017 <- raw2017 |>
+### world bank data
+# Filter for relevant countries and years
+cleaned_wbd_spending <- raw_spending |>
   clean_names() |>
-  select(-c(1:4, 7, 11:24), -extract_date) |>
-  mutate(across(c(6:31), ~as.numeric(str_remove(., "%"))))
-
-# make OSSLT 2018 data readable
-clean2018 <- raw2018 |>
-  clean_names() |>
-  select(-c(1:4, 7, 11:24), -extract_date) |>
-  mutate(across(c(6:31), ~as.numeric(str_remove(., "%"))))
-
-# make OSSLT 2019 data readable
-clean2019 <- raw2019 |>
-  clean_names() |>
-  select(-c(1:4, 7, 11:24), -extract_date) |>
-  mutate(across(c(6:31), ~as.numeric(str_remove(., "%"))))
-
-# make OSSLT 2020 data readable
-clean2020 <- raw2020 |>
-  clean_names() |>
-  select(-c(1:4, 7, 11:24), -extract_date) |>
-  mutate(across(c(6:31), ~as.numeric(str_remove(., "%"))))
-
-# make OSSLT 2021 data readable
-clean2021 <- raw2021 |>
-  clean_names() |>
-  select(-c(1:4, 7, 11:24), -extract_date) |>
-  mutate(across(c(6:29), ~as.numeric(str_remove(., "%"))))
-
-
-# filter cleaned 2017 set for OSSLT-related and relevant data
-osslt_2017 <- clean2017 |>
-  select(
-    school_name, school_type,
-    percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    percentage_of_school_aged_children_who_live_in_low_income_households,
-    percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    percentage_of_students_whose_first_language_is_not_english,
-    percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  rename(osslt10 = percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    ossltchange = change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    low_inc = percentage_of_school_aged_children_who_live_in_low_income_households,
-    no_deg = percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    esl = percentage_of_students_whose_first_language_is_not_english,
-    imm = percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  filter(school_type == "Public" | school_type == "Catholic") |>
-  na.omit()
-
-# filter cleaned 2018 set for OSSLT-related and relevant data
-osslt_2018 <- clean2018 |>
-  select(
-    school_name, school_type,
-    percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    percentage_of_school_aged_children_who_live_in_low_income_households,
-    percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    percentage_of_students_whose_first_language_is_not_english,
-    percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  rename(osslt10 = percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    ossltchange = change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    low_inc = percentage_of_school_aged_children_who_live_in_low_income_households,
-    no_deg = percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    esl = percentage_of_students_whose_first_language_is_not_english,
-    imm = percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  filter(school_type == "Public" | school_type == "Catholic") |>
-  na.omit()
-
-
-# filter cleaned 2019 set for OSSLT-related and relevant data
-osslt_2019 <- clean2019 |>
-  select(
-    school_name, school_type,
-    percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    percentage_of_school_aged_children_who_live_in_low_income_households,
-    percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    percentage_of_students_whose_first_language_is_not_english,
-    percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  rename(osslt10 = percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    ossltchange = change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    low_inc = percentage_of_school_aged_children_who_live_in_low_income_households,
-    no_deg = percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    esl = percentage_of_students_whose_first_language_is_not_english,
-    imm = percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  filter(school_type == "Public" | school_type == "Catholic") |>
-  na.omit()
-
-
-# filter cleaned 2020 set for OSSLT-related and relevant data
-osslt_2020 <- clean2020 |>
-  select(
-    school_name, school_type,
-    percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    percentage_of_school_aged_children_who_live_in_low_income_households,
-    percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    percentage_of_students_whose_first_language_is_not_english,
-    percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  rename(osslt10 = percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    ossltchange = change_in_grade_10_osslt_literacy_achievement_over_three_years,
-    low_inc = percentage_of_school_aged_children_who_live_in_low_income_households,
-    no_deg = percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    esl = percentage_of_students_whose_first_language_is_not_english,
-    imm = percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  filter(school_type == "Public" | school_type == "Catholic") |>
-  na.omit()
-
-
-# filter cleaned 2021 set for OSSLT-related and relevant data
-osslt_2021 <- clean2021 |>
-  select(
-    school_name, school_type,
-    percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    percentage_of_school_aged_children_who_live_in_low_income_households,
-    percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    percentage_of_students_whose_first_language_is_not_english,
-    percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  rename(osslt10 = percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt,
-    low_inc = percentage_of_school_aged_children_who_live_in_low_income_households,
-    no_deg = percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate,
-    esl = percentage_of_students_whose_first_language_is_not_english,
-    imm = percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country
-  ) |>
-  filter(school_type == "Public" | school_type == "Catholic") |>
-  na.omit()
-
-
-### clean canadian government spending data
-
-
-
-
-
-
-
-
-# write data to parquet file
-write_parquet(osslt_2017, "./data/analysis_data/2017.csv")
-write_parquet(osslt_2018, "./data/analysis_data/2018.csv")
-write_parquet(osslt_2019, "./data/analysis_data/2019.csv")
-write_parquet(osslt_2020, "./data/analysis_data/2020.csv")
-write_parquet(osslt_2021, "./data/analysis_data/2021.csv")
-write_parquet(can_math, "./data/analysis_data/canada_math_scores.csv")
-write_parquet(can_read, "./data/analysis_data/canada_literacy_scores.csv")
-write_parquet(can_science, "./data/analysis_data/canada_science_scores.csv")
-write_parquet(chl_math, "./data/analysis_data/chile_math_score.csv")
-write_parquet(chl_read, "./data/analysis_data/chile_literacy_scores.csv")
-write_parquet(chl_science, "./data/analysis_data/chile_science_scores.csv")
-write_parquet(hkg_math, "./data/analysis_data/hongkong_math_scores.csv")
-write_parquet(hkg_read, "./data/analysis_data/hongkong_literacy_scores.csv")
-write_parquet(hkg_science, "./data/analysis_data/hongkong_science_scores.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### graph OSSLT data
-# 2017
-ggplot(osslt_2017, aes(x = osslt10)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of OSSLT Scores in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2017, aes(x = low_inc)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of low income families by % in 2018",
-    x = "coming from low_income (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2017, aes(x = no_deg)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2017, aes(x = imm)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2017, aes(x = esl)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-# 2018
-ggplot(osslt_2018, aes(x = osslt10)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of OSSLT Scores in 2018",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2018, aes(x = low_inc)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of low income families by % in 2018",
-    x = "coming from low_income (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2018, aes(x = no_deg)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2018, aes(x = imm)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2018, aes(x = esl)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-# 2019
-ggplot(osslt_2019, aes(x = osslt10)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of OSSLT Scores in 2019",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2019, aes(x = low_inc)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of low income families by % in 2018",
-    x = "coming from low_income (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2019, aes(x = no_deg)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2019, aes(x = imm)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2019, aes(x = esl)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-# 2020
-ggplot(osslt_2020, aes(x = osslt10)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of OSSLT Scores in 2019",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2020, aes(x = low_inc)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of low income families by % in 2018",
-    x = "coming from low_income (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2020, aes(x = no_deg)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2020, aes(x = imm)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2020, aes(x = esl)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-# 2021
-ggplot(osslt_2021, aes(x = osslt10)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of OSSLT Scores in 2019",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2021, aes(x = low_inc)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of low income families by % in 2018",
-    x = "coming from low_income (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2021, aes(x = no_deg)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2021, aes(x = imm)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
-
-ggplot(osslt_2021, aes(x = esl)) +
-  geom_histogram(bins = 30, fill = "blue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Distribution of Parents with no degree in 2017",
-    x = "OSSLT Scores (%)",
-    y = "Count"
-  ) +
-  facet_grid(~ school_type)
+  select(-indicator_name, -indicator_code, -x2023, -x69) |>
+  filter(country_code == "CAN" |
+           country_code == "CHL" |
+           country_code == "FIN" |
+           country_code == "HKG") |>
+  select(where(~ !anyNA(.)))
+
+
+
+# write data to csv file
+write_csv(can_math, "./data/analysis_data/csv_data/canada_math_scores.csv")
+write_csv(can_read, "./data/analysis_data/csv_data/canada_literacy_scores.csv")
+write_csv(can_science, "./data/analysis_data/csv_data/canada_science_scores.csv")
+write_csv(chl_math, "./data/analysis_data/csv_data/chile_math_score.csv")
+write_csv(chl_read, "./data/analysis_data/csv_data/chile_literacy_scores.csv")
+write_csv(chl_science, "./data/analysis_data/csv_data/chile_science_scores.csv")
+write_csv(hkg_math, "./data/analysis_data/csv_data/hongkong_math_scores.csv")
+write_csv(hkg_read, "./data/analysis_data/csv_data/hongkong_literacy_scores.csv")
+write_csv(hkg_science, "./data/analysis_data/csv_data/hongkong_science_scores.csv")
+write_csv(fin_math, "./data/analysis_data/csv_data/finland_math_scores.csv")
+write_csv(fin_read, "./data/analysis_data/csv_data/finland_literacy_scores.csv")
+write_csv(fin_science, "./data/analysis_data/csv_data/finland_science_scores.csv")
+write_csv(cleaned_gov_spending_data, "./data/analysis_data/csv_data/cleaned_gov_spending_data.csv")
+write_csv(cleaned_wbd_spending, "./data/analysis_data/csv_data/wbd_spending.csv")
+
+
+# write data to parquet
+write_parquet(
+  can_math,
+  "./data/analysis_data/parquet_data/canada_math_scores.csv",
+  chunk_size = NULL,
+  version = "2.4",
+  compression = default_parquet_compression(),
+  compression_level = NULL,
+  use_dictionary = NULL,
+  write_statistics = NULL,
+  data_page_size = NULL,
+  use_deprecated_int96_timestamps = FALSE,
+  coerce_timestamps = NULL,
+  allow_truncated_timestamps = FALSE
+)
+
+
+# Display federal spending as a knitr table
+kable(cleaned_gov_spending_data, caption = "Canadian Government Spending on High School Education")
